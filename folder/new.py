73e1e61b08aead340c82651a6af8e41fbb464062
JanetAdedosu <adedosuadeola71@gmail.com>
2026-04-13 13:48:08 +0200

    Average waiting at B: 0.125 Average waiting at C: 2.275

--- folder/new.py
+++ folder/new.py
@@ -1,28 +1,33 @@
 import random
 
 A_time = 5
-B_time = 6
+B_time = 8
 C_time = 6
 
 products = 40
 
 
-time = 0
+
 A_free = 0
 B_free = 0
 C_free = 0
 
+waiting_B = []
+waiting_C = []
+
 
 for i in range(products):
     # Machine A
-    start_A = max(time, A_free)
+    start_A =  A_free
     finish_A = start_A + A_time
 
     # Machine B
+    wait_B = max(0, finish_A - B_free)
     start_B = max(finish_A, B_free)
     finish_B = start_B + B_time
 
     # Machine C
+    wait_C = max(0, finish_B - C_free)
     start_C = max(finish_B, C_free)
     finish_C = start_C + C_time
 
@@ -31,10 +36,14 @@ for i in range(products):
     B_free = finish_B
     C_free = finish_C
 
-    time = start_A
+    waiting_B.append(wait_B)
+    waiting_C.append(wait_C)
 
 print("Total production time:", finish_C)
 print(f"Product {i+1} finished at time {finish_C}")
+
+print("Average waiting at B:", sum(waiting_B)/len(waiting_B))
+print("Average waiting at C:", sum(waiting_C)/len(waiting_C))
 
 # now it takes 251 seconds to produce all products thru A-B-C , 
 #machine b was the slowest and create a bottle necks 
